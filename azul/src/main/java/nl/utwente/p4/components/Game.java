23b421121ca2e3fd9ea6a53d6e88332eb474f00d
Amadeus Darren Leander <amadeusdarrenleander@gmail.com>
2023-05-03 02:10:28 +0200

    make empty game class

---
+++ azul/src/main/java/nl/utwente/p4/components/Game.java
@@ -0,0 +1,10 @@
+package nl.utwente.p4.components;
+
+import java.util.ArrayList;
+
+public class Game {
+    private TileBag tileBag;
+    private TileTable tileTable;
+    private ArrayList<Factory> factories;
+    private ArrayList<Player> players;
+}
