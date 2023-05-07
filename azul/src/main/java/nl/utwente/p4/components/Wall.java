6bdaaf9c3fe0092687f588ea96e47294be6732aa
chaka <chakaroun_ali@yahoo.com>
2023-05-07 14:43:24 +0300

    change tiles form arraylist to hashmaps, reason is to beable to use key-value pairs inorder to properly store the tiles. created the logic for create a wall.

--- azul/src/main/java/nl/utwente/p4/components/Wall.java
+++ azul/src/main/java/nl/utwente/p4/components/Wall.java
@@ -1,23 +1,40 @@
 package nl.utwente.p4.components;
 
-import java.util.ArrayList;
+import nl.utwente.p4.constants.TileType;
+
+import java.util.*;
 
 public class Wall {
-    private ArrayList<ArrayList<Tile>> tiles;
-
-    public ArrayList<ArrayList<Tile>> getTiles() {
+private Map<TileType, TileType>[] tiles;
+    public Map<TileType, TileType>[] getTiles() {
         return tiles;
     }
-
-    public void setTiles(ArrayList<ArrayList<Tile>> tiles) {
+    public void setTiles(Map<TileType, TileType>[] tiles) {
         this.tiles = tiles;
     }
-
+    public TileType[] WallSet() {
+        TileType[] set = {TileType.BLUE,TileType.YELLOW,TileType.RED,TileType.BLACK,TileType.WHITE};
+        return set;
+    }
+    public static TileType[] RotateSet(TileType[] set){
+        //move set one index to the right
+        TileType lastSetElement = set[set.length - 1];
+        System.arraycopy(set, 0, set, 1, set.length - 1);
+        set[0] = lastSetElement;
+        return set;
+    }
     public Wall() {
-        this.tiles = new ArrayList<>();
+        this.tiles = new HashMap[5];
+        TileType[] set = WallSet();
+
         for (int i = 0; i < 5; i++) {
-            this.tiles.add(new ArrayList<>());
+            this.tiles[i] = new LinkedHashMap<>();
+            for (TileType key : set){
+                this.tiles[i].put(key, null);
+            }
+            set = RotateSet(set);
         }
+        setTiles(this.tiles);
     }
 
     // TODO: implement method
--- azul/src/main/java/nl/utwente/p4/constants/TileType.java
+++ azul/src/main/java/nl/utwente/p4/constants/TileType.java
@@ -3,6 +3,7 @@ package nl.utwente.p4.constants;
 public enum TileType {
     BLACK,
     GREEN,
+    WHITE,
     BLUE,
     YELLOW,
     RED,
