6d5f001930682c41c9eb06584dc261b4b2cfe155
Amadeus Darren Leander <amadeusdarrenleander@gmail.com>
2023-05-10 01:01:07 +0200

    revert

--- azul/src/main/java/nl/utwente/p4/Main.java
+++ azul/src/main/java/nl/utwente/p4/Main.java
@@ -4,7 +4,6 @@ import nl.utwente.p4.components.Game;
 
 public class Main {
     public static void main(String[] args) {
-        testing123
         System.out.println("Azul");
         new Game().play();
     }
