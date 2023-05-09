e884b1f9b64e06c1d2108eddb41dd3baadbd8e18
Dheetchithaa Selvaraj <d.selvaraj@student.utwente.nl>
2023-05-09 17:02:58 +0200

    updates

--- azul/src/main/java/nl/utwente/p4/components/Game.java
+++ azul/src/main/java/nl/utwente/p4/components/Game.java
@@ -113,29 +113,5 @@ public class Game {
     public void prepareNextRound() {}
 
     // TODO: implement method
-    public void endGame() {
-        boolean hasGameEnded = false;
-        // TODO: iterate through players arraylist to check if anyone has a row filled out
-        for (Player p : players) {
-            if (p.hasFilledRow()) {
-                hasGameEnded = true;
-                break;
-            }
-        }
-
-        if (!hasGameEnded) {
-            return;
-        }
-
-        int highestScore = -99999;
-        // TODO: call the calcuate score function for each player & check player with the highest score
-        for (Player p : players) {
-            int score = p.calculateScore();
-
-            // check if this score was highest
-            if (score > highestScore) {
-                highestScore = score;
-            }
-        }
-    }
+    public void endGame() {}
 }
