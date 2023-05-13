2fa206f3ec0d51c5a75207d6ff5c04fc67e0e324
Amadeus Darren Leander <amadeusdarrenleander@gmail.com>
2023-05-13 13:32:06 +0200

    refactor numOfPlayers and add unit tests to Game class

--- azul/src/main/java/nl/utwente/p4/Main.java
+++ azul/src/main/java/nl/utwente/p4/Main.java
@@ -5,6 +5,6 @@ import nl.utwente.p4.components.Game;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Azul");
-        Game.getInstance().play();
+        Game.getInstance().play(2);
     }
 }
--- azul/src/test/java/nl/utwente/p4/GameTest.java
+++ azul/src/test/java/nl/utwente/p4/GameTest.java
@@ -1,23 +1,127 @@
 package nl.utwente.p4;
 
+import nl.utwente.p4.components.Factory;
 import nl.utwente.p4.components.Game;
+import nl.utwente.p4.components.Tile;
+import nl.utwente.p4.components.TileBag;
+import nl.utwente.p4.constants.TileType;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class GameTest {
     @Test
-    void startGame_createdTwoPlayers_true() { // methodName_behaviorToBeTested_expectedResult
+    void startGame_gameSetup_true() { // methodName_behaviorToBeTested_expectedResult
         // arrange
         Game game = Game.getInstance();
+        game.setPlayers(new ArrayList<>());
+        game.setTileBag(new TileBag());
+        game.setFactories(new ArrayList<>());
+        game.setNumOfPlayers(2);
+        int numOfFactories = 2 * game.getNumOfPlayers() + 1;
 
         // act
         game.startGame();
 
         // assert
         // reference for assertion methods: https://junit.org/junit5/docs/5.0.1/api/org/junit/jupiter/api/Assertions.html
+        // assert player
         assertNotNull(game.getPlayers());
-        assertEquals(2, game.getPlayers().size());
+        assertEquals(game.getNumOfPlayers(), game.getPlayers().size());
+
+        // assert tile bag
+        assertNotNull(game.getTileBag());
+        assertNotNull(game.getTileBag().getTiles());
+        assertEquals(100 - (4 * numOfFactories), game.getTileBag().getTiles().size());  // not 100 bcos 20 have been added to factories
+
+        // assert factories
+        assertNotNull(game.getFactories());
+        assertEquals(numOfFactories, game.getFactories().size());
+    }
+
+    @Test
+    void prepareNextRound_resetFirstHasBeenTaken_firstHasBeenTakenFalse() {
+        // arrange
+        Game game = Game.getInstance();
+        game.getTileTable().setFirstHasBeenTaken(true);
+
+        // act
+        game.prepareNextRound();
+
+        // assert
+        // reference for assertion methods: https://junit.org/junit5/docs/5.0.1/api/org/junit/jupiter/api/Assertions.html
+        assertFalse(game.getTileTable().isFirstHasBeenTaken());
+    }
+
+    @Test
+    void prepareNextRound_refillFactories_completelyRefilled() {
+        // arrange
+        Game game = Game.getInstance();
+        int numOfFactories = 5;
+
+        TileBag tileBag = new TileBag();
+        for (int i = 0; i < numOfFactories * 4; i++) {
+            tileBag.getTiles().add(new Tile(TileType.BLACK));
+        }
+        game.setTileBag(tileBag);
+
+        ArrayList<Factory> factories = new ArrayList<>();
+        for (int i = 0; i < numOfFactories; i++) {
+            factories.add(new Factory(new ArrayList<>()));
+        }
+        game.setFactories(factories);
+
+        // act
+        game.prepareNextRound();
+
+        // assert
+        // reference for assertion methods: https://junit.org/junit5/docs/5.0.1/api/org/junit/jupiter/api/Assertions.html
+        // assert tile bag
+        assertEquals(0, game.getTileBag().getTiles().size());
+
+        // assert factories
+        for (int i = 0; i < game.getFactories().size(); i++) {
+            assertEquals(4, game.getFactories().get(i).getTiles().size());
+        }
+    }
+
+    @Test
+    void prepareNextRound_refillFactories_notEnoughTiles() {
+        // arrange
+        Game game = Game.getInstance();
+        int numOfFactories;
+
+        numOfFactories = 2;
+        TileBag tileBag = new TileBag();
+        for (int i = 0; i < numOfFactories * 4; i++) {
+            tileBag.getTiles().add(new Tile(TileType.BLACK));
+        }
+        game.setTileBag(tileBag);
+
+        numOfFactories = 5;
+        ArrayList<Factory> factories = new ArrayList<>();
+        for (int i = 0; i < numOfFactories; i++) {
+            factories.add(new Factory(new ArrayList<>()));
+        }
+        game.setFactories(factories);
+
+        // act
+        game.prepareNextRound();
+
+        // assert
+        // reference for assertion methods: https://junit.org/junit5/docs/5.0.1/api/org/junit/jupiter/api/Assertions.html
+        // assert tile bag
+        assertEquals(0, game.getTileBag().getTiles().size());
+
+        // assert factories
+        for (int i = 0; i < 2; i++) {
+            assertEquals(4, game.getFactories().get(i).getTiles().size());
+        }
+        for (int i = 2; i < game.getFactories().size(); i++) {
+            assertEquals(0, game.getFactories().get(i).getTiles().size());
+        }
     }
 }
