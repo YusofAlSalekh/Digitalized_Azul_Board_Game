9efa6d57752be6fe862089c33a0e773492df37ba
PaavoNykanen <paavonykanen@gmail.com>
2023-05-17 16:43:34 +0200

    Refactored floorline addTile to add excess to gameboxlid
    - Refactore floorline addTile to add excess tiles to game box lid
    - Removed the check and adding of excess tiles to the game box lid from the player addTiles method

--- azul/src/test/java/nl/utwente/p4/FloorLineTest.java
+++ azul/src/test/java/nl/utwente/p4/FloorLineTest.java
@@ -6,6 +6,8 @@ import nl.utwente.p4.components.Tile;
 import nl.utwente.p4.constants.TileType;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
@@ -17,16 +19,17 @@ public class FloorLineTest {
         Tile tile = new Tile(TileType.BLACK);
 
         // act
-        Boolean wasAdded = floorLine.addTile(tile);
+        floorLine.addTile(tile);
 
         // assert
-        assertEquals(true, wasAdded);
         assertEquals(1, floorLine.getTiles().size());
     }
 
     @Test
     void addTile_tileAdded_false() { // methodName_behaviorToBeTested_expectedResult
         // arrange
+        Game game = Game.getInstance();
+        game.getTileBag().setTiles(new ArrayList<Tile>());
         FloorLine floorLine = new FloorLine();
         Tile tile1 = new Tile(TileType.BLACK);
         Tile tile2 = new Tile(TileType.BLACK);
@@ -38,19 +41,18 @@ public class FloorLineTest {
         Tile tile8 = new Tile(TileType.BLACK);
 
         // act
-        Boolean wasAdded1 = floorLine.addTile(tile1);
-        Boolean wasAdded2 = floorLine.addTile(tile2);
-        Boolean wasAdded3 = floorLine.addTile(tile3);
-        Boolean wasAdded4 = floorLine.addTile(tile4);
-        Boolean wasAdded5 = floorLine.addTile(tile5);
-        Boolean wasAdded6 = floorLine.addTile(tile6);
-        Boolean wasAdded7 = floorLine.addTile(tile7);
-        Boolean wasAdded8 = floorLine.addTile(tile8);
+        floorLine.addTile(tile1);
+        floorLine.addTile(tile2);
+        floorLine.addTile(tile3);
+        floorLine.addTile(tile4);
+        floorLine.addTile(tile5);
+        floorLine.addTile(tile6);
+        floorLine.addTile(tile7);
+        floorLine.addTile(tile8);
 
         // assert
-        assertEquals(true, wasAdded7);
-        assertEquals(false, wasAdded8);
         assertEquals(7, floorLine.getTiles().size());
+        assertEquals(1, Game.getInstance().getGameBoxLid().getTiles().size());
     }
 
     @Test
