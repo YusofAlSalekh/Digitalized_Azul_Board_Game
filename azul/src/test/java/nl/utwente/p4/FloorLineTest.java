b07d11888ab8353408eec57425e9611b3c7ddc0b
chaka <chakaroun_ali@yahoo.com>
2023-05-25 18:17:44 +0200

    fix for the game.getinstance() fail in FloorLineTest when running gameTest class before it.

--- azul/src/test/java/nl/utwente/p4/FloorLineTest.java
+++ azul/src/test/java/nl/utwente/p4/FloorLineTest.java
@@ -29,6 +29,10 @@ public class FloorLineTest {
     void addTile_tileAdded_false() { // methodName_behaviorToBeTested_expectedResult
         // arrange
         Game game = Game.getInstance();
+        // set the Lid to an empty array since  FloorLinetest class conflicts with the
+        // Gametest class when running all tests/
+        ArrayList<Tile> empty = new ArrayList<>();
+        game.getGameBoxLid().setTiles(empty);
         game.getTileBag().setTiles(new ArrayList<Tile>());
         FloorLine floorLine = new FloorLine();
         Tile tile1 = new Tile(TileType.BLACK);
@@ -52,7 +56,7 @@ public class FloorLineTest {
 
         // assert
         assertEquals(7, floorLine.getTiles().size());
-        assertEquals(1, Game.getInstance().getGameBoxLid().getTiles().size());
+        assertEquals(1, game.getGameBoxLid().getTiles().size());
     }
 
     @Test
