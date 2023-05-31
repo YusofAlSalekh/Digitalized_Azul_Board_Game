8532ab449b193e03fac3fa8a091bcc3b2b2fa534
Iusof <noggano060571noggano@mail.ru>
2023-05-31 17:12:21 +0200

    add javadoc

--- azul/src/main/java/nl/utwente/p4/components/TileLineFactory.java
+++ azul/src/main/java/nl/utwente/p4/components/TileLineFactory.java
@@ -4,7 +4,7 @@ import nl.utwente.p4.gamestate.TileLineAdapter;
 
 public class TileLineFactory {
     public static GeneralTileLine createTileLine(int size) {
-        if (System.getProperty("externalTileLine") != null) {
+        if (System.getProperties().containsKey("externalTileLine")) {
             return new TileLineAdapter(size);
         } else {
             return new TileLine(size);
