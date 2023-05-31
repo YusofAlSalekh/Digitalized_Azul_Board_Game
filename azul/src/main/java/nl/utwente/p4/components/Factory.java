a6a0a2361acca1028590d32bd97d435448dc57a9
Iusof <noggano060571noggano@mail.ru>
2023-05-31 19:09:03 +0200

    yusof_refactoring

--- azul/src/main/java/nl/utwente/p4/components/Factory.java
+++ azul/src/main/java/nl/utwente/p4/components/Factory.java
@@ -43,6 +43,8 @@ public class Factory {
 
     //Tiles should also be removed from the factory, instead of just returning them.
     public ArrayList<Tile> getRemainingTiles() {
-        return tiles;
+        ArrayList<Tile> temp = tiles;
+        tiles.clear();
+        return temp;
     }
 }
