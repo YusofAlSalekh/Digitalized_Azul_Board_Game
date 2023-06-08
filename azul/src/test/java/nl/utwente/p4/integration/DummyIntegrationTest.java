7ba3bd0a02d51746705a7cd11af71078e2e6f85c
Amadeus Darren Leander <amadeusdarrenleander@gmail.com>
2023-06-08 17:36:01 +0200

    add dummy assert

--- azul/src/test/java/nl/utwente/p4/integration/DummyIntegrationTest.java
+++ azul/src/test/java/nl/utwente/p4/integration/DummyIntegrationTest.java
@@ -2,9 +2,11 @@ package nl.utwente.p4.integration;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class DummyIntegrationTest {
     @Test
     void dummyIntegrationTest() {
-
+        assertTrue(true);
     }
 }
--- azul/src/test/java/nl/utwente/p4/system/DummySystemTest.java
+++ azul/src/test/java/nl/utwente/p4/system/DummySystemTest.java
@@ -2,9 +2,11 @@ package nl.utwente.p4.system;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class DummySystemTest {
     @Test
     void dummySystemTest() {
-
+        assertTrue(true);
     }
 }
