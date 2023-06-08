bb7404ff54d98da692d0eb8a3c3cd5accd7dbfc1
Amadeus Darren Leander <amadeusdarrenleander@gmail.com>
2023-06-08 17:21:46 +0200

    add dummy integartion and system test

---
+++ azul/src/test/java/nl/utwente/p4/integration/DummyIntegrationTest.java
@@ -0,0 +1,10 @@
+package nl.utwente.p4.integration;
+
+import org.junit.jupiter.api.Test;
+
+public class DummyIntegrationTest {
+    @Test
+    void dummyIntegrationTest() {
+
+    }
+}
---
+++ azul/src/test/java/nl/utwente/p4/system/DummySystemTest.java
@@ -0,0 +1,10 @@
+package nl.utwente.p4.system;
+
+import org.junit.jupiter.api.Test;
+
+public class DummySystemTest {
+    @Test
+    void dummySystemTest() {
+
+    }
+}
