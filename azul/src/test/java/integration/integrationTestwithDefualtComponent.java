8982f8a665a86d954cfea4a32111238aff116841
Iusof <noggano060571noggano@mail.ru>
2023-06-14 16:36:25 +0200

    integration Tests

--- azul/src/test/java/integration/integrationTestwithDefualtComponent.java
+++ azul/src/test/java/integration/integrationTestwithDefualtComponent.java
@@ -1,9 +1,12 @@
 package integration;
 
+import nl.utwente.p4.components.Factory;
 import nl.utwente.p4.components.Game;
 import nl.utwente.p4.components.PatternLine;
 import nl.utwente.p4.components.Tile;
 import nl.utwente.p4.constants.TileType;
+import nl.utwente.p4.exceptions.TileColourNotMatchedException;
+import nl.utwente.p4.exceptions.TileColourNotMatchedWallTileColourException;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -96,4 +99,65 @@ public class integrationTestwithDefualtComponent {
         assertEquals(TileType.NULL, p1.getPatternLine().getTileLines().get(1).getLineType());
         assertTrue(p1.getWall().isTileFilled(new Tile(TileType.RED),1));
     }
+
+    @Test
+    void test1() {
+        Game game = Game.getInstance();
+        game.getPlayers().clear();
+        game.setNumOfPlayers(2);
+        game.setTileLineIsExternal(false);
+        game.startGame();
+        var p1 = game.getPlayers().get(0);
+
+        ArrayList<Tile> tiles1 = new ArrayList<>();
+        tiles1.add(new Tile(TileType.RED));
+        tiles1.add(new Tile(TileType.RED));
+
+        // add two red tiles to the second row
+        p1.getPatternLine().addTiles(tiles1,2);
+
+        // check that there are 2 tiles in the second row
+        assertEquals(2,p1.getPatternLine().getTileLines().get(2).getTiles().size());
+
+        ArrayList<Tile> tiles2 = new ArrayList<>();
+        tiles2.add(new Tile(TileType.BLACK));
+        tiles2.add(new Tile(TileType.BLACK));
+
+        // check that there will be an exception if we try to add black tiles to red tiles
+        assertThrows(TileColourNotMatchedException.class,
+                () -> p1.getPatternLine().addTiles(tiles2, 2));
+
+    }
+
+    @Test
+    void tes2() {
+        Game game = Game.getInstance();
+        game.getPlayers().clear();
+        game.setNumOfPlayers(2);
+        game.setTileLineIsExternal(false);
+        game.startGame();
+        var p1 = game.getPlayers().get(0);
+
+        ArrayList<Tile> tiles1 = new ArrayList<>();
+        tiles1.add(new Tile(TileType.RED));
+        tiles1.add(new Tile(TileType.RED));
+
+        p1.getPatternLine().addTiles(tiles1,2);
+
+        assertEquals(2,p1.getPatternLine().getTileLines().get(2).getTiles().size());
+
+        p1.getWall().addFromPatterLineToWall(p1.getPatternLine(),1);
+
+        ArrayList<Tile> tiles = new ArrayList<>();
+        Factory factory = new Factory(tiles);
+        tiles.add(new Tile(TileType.BLACK));
+        tiles.add(new Tile(TileType.YELLOW));
+        tiles.add(new Tile(TileType.RED));
+        factory.setTiles(tiles);
+
+
+        assertThrows(TileColourNotMatchedWallTileColourException.class,
+                () -> p1.getFactoryOfferFromFactory(factory,TileType.RED,2));
+    }
+
 }
